7ad52067e56237ee8dd2ea644a3cd4474886610b
Ruthtadesse <ruthtadesse4998@gmail.com>
2024-05-04 16:07:05 +0300

    signup and login frontend

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,8 +1,121 @@
 import React from 'react'
-
+import AppleIcon from "@mui/icons-material/Apple";
+import GoogleIcon from "@mui/icons-material/Google";
+import FacebookIcon from "@mui/icons-material/Facebook";
 const LoginPage = () => {
   return (
-    <div>Login</div>
-  )
+    <div className="flex items-center justify-center min-h-screen ">
+      <div className="w-full max-w-2xl">
+        <h1 className=" font-bold ml-4 mb-8 text-2xl text-gray-800">
+          Log in to your Udemy account
+        </h1>
+        <form className="space-y-4 ">
+          <div className="w-full max-w-2xl">
+            <div className=" flex j border border-black overflow-hidden transition-all duration-300">
+              <div>
+                <img src={GoogleIcon} alt="" />
+              </div>
+
+              <button
+                type="button"
+                id="google-signin"
+                className="w-full px-3 py-6 border rounded focus:outline-none hover:bg-gray-200 hover:border-transparent font-bold text-3xl text-gray-800"
+              >
+                Continue with Google
+              </button>
+            </div>
+          </div>
+
+          <div className="w-full max-w-2xl">
+            <div className=" flex j border border-black overflow-hidden transition-all duration-300">
+              <div>
+                <img src={FacebookIcon} alt="" />
+              </div>
+
+              <button
+                type="button"
+                id="google-signin"
+                className="w-full px-3 py-6 border rounded focus:outline-none hover:bg-gray-200 hover:border-transparent font-bold text-3xl text-gray-800"
+              >
+                Continue with FaceBook
+              </button>
+            </div>
+          </div>
+
+          <div className="w-full max-w-2xl">
+            <div className=" flex j border border-black overflow-hidden transition-all duration-300">
+              <div>
+                <img src={AppleIcon} alt="" />
+              </div>
+
+              <button
+                type="button"
+                id="google-signin"
+                className="w-full px-3 py-6 border rounded focus:outline-none hover:bg-gray-200 hover:border-transparent font-bold text-3xl text-gray-800"
+              >
+                Continue with Apple
+              </button>
+            </div>
+          </div>
+          <div className=" border  px-4  border-black  overflow-hidden   ">
+            <label htmlFor="email" className="block mb-1 pt-8  font-bold  ">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="w-full border rounded px-3  focus:outline-none"
+            />
+          </div>
+          <div className="  border  px-6 border-black  overflow-hidden">
+            <label htmlFor="password" className="block  pt-8  mb-1 font-bold ">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="w-full border rounded px-3 focus:outline-none "
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="w-full bg-purple-600 text-white py-5 font-bold rounded mb-12 hover:bg-purple-700"
+          >
+            Log in
+          </button>
+        </form>
+        <div className="flex justify-center px-2 space-x-4 mb-4 mt-4">
+          <div className=" text-gray-800">or</div>
+          <div className="text-purple-800 font-bold underline hover:text-purple-900 ">
+            Forgot Password
+          </div>
+        </div>
+        <div>
+          <hr />
+        </div>
+        <p className="text-center mt-6">
+          Don't have an account?{" "}
+          <a
+            href="/signup"
+            className="text-purple-700 font-bold underline hover:text-purple-900 "
+          >
+            sign up
+          </a>
+          <p>
+            <a
+              href="/login"
+              className="text-purple-700 font-bold text-center underline hover:text-purple-900"
+            >
+              Log in with your organization
+            </a>
+          </p>
+        </p>
+      </div>
+    </div>
+  );
+  
 }
 export default LoginPage
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,8 +1,89 @@
-import React from 'react'
+import React from "react";
 
-const SignUpPage = () => {
+const Signup = () => {
+  
   return (
-    <div>SignUp</div>
-  )
-}
-export default SignUpPage
+    <div className="flex items-center justify-center min-h-screen font-sans">
+      <div className="w-full max-w-2xl">
+        <h1 className="text-3xl font-bold ml-4 mb-8">
+          Sign up and start learning
+        </h1>
+        <form className="space-y-4 ">
+          <div className="border  px-6 border-black overflow-hidden transition-all duration-300">
+            <label htmlFor="fullname" className="block mb-1 pt-8 font-semibold">
+              Full name
+            </label>
+            <input
+              type="text"
+              id="fullname"
+              name="fullname"
+              className="w-full px-3   focus:outline-none"
+            />
+          </div>
+
+          <div className=" border  px-6 border-black  overflow-hidden   ">
+            <label htmlFor="email" className="block mb-1 pt-8  font-semibold  ">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="w-full border rounded px-3  focus:outline-none"
+            />
+          </div>
+          <div className="  border  px-6 border-black  overflow-hidden">
+            <label
+              htmlFor="password"
+              className="block  pt-8  mb-1 font-semibold "
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="w-full border rounded px-3 focus:outline-none "
+            />
+          </div>
+
+          <div>
+            <input
+              type="checkbox"
+              id="special-offers"
+              name="special-offers"
+              className="mr-2  ml-2 border-black"
+            />
+            <label className="text-2xl  font-normal" htmlFor="special-offers">
+              Send me special offers, personalized recommendations, and learning
+              tips
+            </label>
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-purple-600 text-white py-5 font-bold rounded mb-8 hover:bg-purple-700"
+          >
+            Sign Up
+          </button>
+        </form>
+        <div className="text-center mt-8 text-gray-800 text-lg mb-10">
+          <p>
+            By signing up, you agree to our Terms of use and Privacy Policy.
+          </p>
+        </div>
+        <div>
+          <hr />
+        </div>
+        <p className="text-center mt-4">
+          Already have an account?{" "}
+          <a href="/login" className="text-purple-900 font-bold underline">
+            Log in
+          </a>
+        </p>
+      
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
