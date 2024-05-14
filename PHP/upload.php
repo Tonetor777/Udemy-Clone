9a57ee5cd54f21efdb944540bb7c3903c5029acc
Ruth <ruthzinabu89@gmail.com>
2024-05-14 21:22:20 +0300

    image-upload

--- PHP/upload.php
+++ PHP/upload.php
@@ -1,35 +1,50 @@
 <?php
-$targetDirectory = "uploads/";
-$targetFile = $targetDirectory . basename($_FILES["file"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+ 
 
-// Check if file already exists
-if (file_exists($targetFile)) {
-    echo json_encode(array('error' => 'File already exists.'));
-    $uploadOk = 0;
-}
+header("Access-Control-Allow-Origin: http://localhost:8003");
+header("Access-Control-Allow-Methods: POST");  
+header("Access-Control-Allow-Headers: Content-Type, Authorization");  
+ 
 
-// Check file size
-if ($_FILES["file"]["size"] > 5000000) { 
-    echo json_encode(array('error' => 'File is too large.'));
-    $uploadOk = 0;
-}
 
-// Allow certain file formats
-if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-    && $imageFileType != "gif" && $imageFileType != "pdf") { 
-    echo json_encode(array('error' => 'Only JPG, JPEG, PNG, GIF, PDF files are allowed.'));
-    $uploadOk = 0;
-}
 
-if ($uploadOk == 0) {
-    echo json_encode(array('error' => 'File upload failed.'));
+if (!isset($_FILES["file"])) {
+    echo json_encode(array('error' => 'No file uploaded.'));
 } else {
-    if (move_uploaded_file($_FILES["file"]["tmp_name"], $targetFile)) {
-        echo json_encode(array('success' => 'File uploaded successfully.'));
-    } else {
-        echo json_encode(array('error' => 'File upload failed.'));
+    $targetDirectory = "C:\\xampp\\htdocs\\udemy\\PHP\\uploads\\";
+    $targetFile = $targetDirectory . basename($_FILES["file"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+
+    // Check file size
+    if ($_FILES["file"]["size"] > 10000000) { 
+        echo json_encode(array('error' => 'File is too large.'));
+        $uploadOk = 0;
     }
+
+    // Allow certain file formats
+    $allowedExtensions = array("jpg", "jpeg", "png", "gif", "pdf", "mp4", "mp3");
+    if (!in_array($imageFileType, $allowedExtensions)) {
+        echo json_encode(array('error' => 'Only JPG, JPEG, PNG, GIF, PDF files are allowed.'));
+        $uploadOk = 0;
+    }
+
+    if ($uploadOk == 0) {
+        echo json_encode(array('error' => 'File upload failed.'));
+    } else {
+        if (move_uploaded_file($_FILES["file"]["tmp_name"], $targetFile)) {
+            // File uploaded successfully, store file path in the database
+            echo json_encode(array('success' => 'File uploaded successfully.'
+                                   ));
+                               
+                                   
+        } else {
+            echo json_encode(array('error' => 'File upload failed.'));
+        }
+    }
+    
+
+   
 }
+
 ?>
