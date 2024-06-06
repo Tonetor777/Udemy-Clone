b8000b4f7c1c94038874e5ab723c085546f579c2
robeltt <robel.teshome83@gmail.com>
2024-06-06 20:12:58 +0300

    update success

--- StripApi/success.php
+++ StripApi/success.php
@@ -8,3 +8,19 @@
         <small style="text-align: center;">We hope you will enjoy the courses that we provides and find it life changing. </small>
     </body>
 </html>
+<?php
+require 'dbconfig.php';
+
+loadEnv(__DIR__ . '/.env');
+
+$db = new Database();
+$conn=$db->getConnection();
+
+$sql="insert into Payments values (?,?,?,?,?);";
+$stm=$conn->prepare($sql);
+$stm->bind_param("iiisd",$pay_id,$user_id,$course_id,$amount,$pay_date);
+$timestamp = time();
+
+$date = date('Y-m-d H:i:s', $timestamp);
+$pay_date= $date;
+?>
