965c9927fa58c78aa7f5349065ba81b93b0bec33
salemhabte <habtesalem20@gmail.com>
2024-06-06 11:39:15 -0700

    change

--- StripApi/success.php
+++ StripApi/success.php
@@ -5,5 +5,6 @@
     </head>
     <body>
         <h1 style="text-align: center; color: green;">Thank you for your payment</h1>
+        <small style="text-align: center;">We hope you will enjoy the courses that we provides and find it life changing. </small>
     </body>
 </html>
