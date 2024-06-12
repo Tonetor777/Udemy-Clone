76f07667a9aa02431f84b711eed768df7b25fb07
RuthTadesse <125727910+RuthTadesse@users.noreply.github.com>
2024-06-12 11:39:55 +0300

    Added Session.php

--- PHP/Session.php
+++ PHP/Session.php
@@ -1,1 +1,15 @@
 
+<?php
+session_start();
+
+header("Access-Control-Allow-Origin: http://localhost:8001");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json; charset=UTF-8");
+
+if (isset($_SESSION['user'])) {
+    $user = $_SESSION['user'];
+    echo json_encode(['user' => $user]);
+} else {
+    echo json_encode(['error' => 'User session not found']);
+}
